9349ab57c68506255faa0d0987c487cb31e51287
bdkamaci <burcudogakamaci@gmail.com>
2025-02-25 14:29:04 +0100

    feat: create project structure and configuration

--- src/test/java/com/bdkamaci/unitconverter/UnitConverterApplicationTests.java
+++ src/test/java/com/bdkamaci/unitconverter/UnitConverterApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UnitConverterApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
